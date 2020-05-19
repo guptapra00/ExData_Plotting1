915ed819865a5eff3aa763d60c2f7862d9fcad04
prakhar gupta <64835180+guptapra00@users.noreply.github.com>
2020-05-19 12:55:55 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -23,3 +23,10 @@ t <- cbind(dateTime, t)
   
 ## Format dateTime Column
 t$dateTime <- as.POSIXct(dateTime)
+
+## Create the histogram
+  hist(t$Global_active_power, main="Global Active Power", xlab = "Global Active Power (kilowatts)", col="red")
+
+## Save file and close device
+  dev.copy(png,"plot1.png", width=480, height=480)
+  dev.off()
